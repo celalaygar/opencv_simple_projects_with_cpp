6140fcb40cff1638b0d35bb57ba029f40f9a9275
Celal Aygar <celal.aygar@gmail.com>
2017-09-11 22:13:44 +0300

    Update kamera üzerinde pixel değerini değiştirme.cpp

--- kamera üzerinde pixel değerini değiştirme.cpp
+++ kamera üzerinde pixel değerini değiştirme.cpp
@@ -19,22 +19,9 @@ int main(int argc, char** argv)
 
 	VideoCapture kamera(0);
 	Mat frame, frame_gri;
-
-
-	// detectMultiscale Cascade Classifier Parametreleri
-	int groundThreshold = 2;
-	double scaleStep = 1.1;
-	Size minimumNesneBoyutu(5, 5);
-	Size maksimumNesneBoyutu(250, 250);
-
 	vector<Rect> bulunanlar;
 	while (true) {
 		kamera >> frame;
-
-		cvtColor(frame, frame_gri, CV_BGR2GRAY);
-
-		// Yüzleri algılayalım/bulalım/detect edelim :)
-		dedektor.detectMultiScale(frame_gri, bulunanlar, scaleStep, groundThreshold, CV_HAAR_SCALE_IMAGE, minimumNesneBoyutu, maksimumNesneBoyutu);
 		//resmin ilk 200 x 200 lük bölümünü tek renk haline getirdik
 		for (int x = 0; x < 200;x++) {
 			for (int y = 0; y < 200;y++) {
@@ -43,10 +30,7 @@ int main(int argc, char** argv)
 				frame.at<cv::Vec3b>(x, y)[2] = 0;	   //kırmızı
 			}
 		}
-
-
 		imshow("Yuz Bulucu", frame);
-
 		if (waitKey(3) == 27) break;
 	}
 
