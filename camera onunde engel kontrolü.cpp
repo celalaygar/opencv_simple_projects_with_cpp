d90d433e72c976656e79478d4b87c80af0f704ac
Celal Aygar <celal.aygar@gmail.com>
2017-09-13 01:11:00 +0300

    Update camera onunde engel kontrolü.cpp

--- camera onunde engel kontrolü.cpp
+++ camera onunde engel kontrolü.cpp
@@ -32,28 +32,26 @@ int main(int argc, char** argv)
 		pixel = (frame.rows-200)*(frame.cols-200); cnt = 0;
 		
 		cvtColor(frame, frame_gri, CV_BGR2GRAY);
+		//resmin ilk 200 x 200 lük bölümünü tek renk haline getirdik
 		for (int x = 50; x < frame.rows- 50;x++) {
 			for (int y = 50; y < frame.cols- 50;y++) {
-				int b = 0,c=0,d=0;
-				b = frame.at<cv::Vec3b>(x, y)[0];  //blue
-				c = frame.at<cv::Vec3b>(x, y)[1];  //green
-				d = frame.at<cv::Vec3b>(x, y)[2];  //red
-				if (frame.at<cv::Vec3b>(x, y)[0] < 50 && frame.at<cv::Vec3b>(x, y)[1] < 50 && frame.at<cv::Vec3b>(x, y)[2] < 50) {
-
+				int b = 0,c=0,a=0;
+				a = frame.at<cv::Vec3b>(x, y)[0];  //blue
+				b = frame.at<cv::Vec3b>(x, y)[1];  //green
+				c = frame.at<cv::Vec3b>(x, y)[2];  //red
+				if (b < 18 && b < 18 && c < 18) {
 					cnt++;
 				}
-
 			}
 		}
 		cout <<" tp : "<< ToplamPixel<<" p : "<<pixel<<" tp-p : "<< ToplamPixel - pixel << "   cnt : " <<cnt << endl;
 		if (cnt > pixel-10000 )
-			cout << " Kamera önünde engel var" << endl;
-		
-
-		imshow("Yuz Bulucu", frame);
-
+			cout << " -------------------------------- " << endl;
+		imshow("Görüntü", frame);
 		if (waitKey(3) == 27) break;
 	}
 
 	waitKey(0); // Wait for a keystroke in the window
 	return 0;
+
+}
